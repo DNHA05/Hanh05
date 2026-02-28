1eb42a87f07b744e5909865d48cc58f39ae22428
Hoàng A'nh <159562977+DNHA05@users.noreply.github.com>
2026-03-01 01:32:07 +0700

    Delete Edit directory

--- Edit/schema.ts
+++
@@ -1,9 +0,0 @@
--0
-+6
-  success: boolean;
-  result: string;
-};
-export const statsResponseSchema = z.object({
-  views: z.number(),
-});
-export type StatsResponse = z.infer<typeof statsResponseSchema>;
